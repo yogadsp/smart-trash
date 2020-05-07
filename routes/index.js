113e1bbe9db827abbb2b8063b0c84dd63de50143
yogadsp <yogaseptana@gmail.com>
2020-05-07 22:20:27 +0700

    update post

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer');
+var fs  = require('fs');
+var upload = multer({ dest: '/tmp/'});
+path = require('path');
 
 router.get('/', function(req, res, next) {
   res.render('http_se', { title: 'HTTP RECEIVER' , message : 'This is message'});
-
 });
 
-router.post('/post', function(req, res) {
-    var body = req.body;
-	var isi = req.body.sensor;
-	console.log("Isi " + isi);
-	console.log("Body " + body.toString());
+router.post('/post', upload.single('file'), function(req, res) {
+  var file = path.join(__dirname, '../public/capturedphotos/', req.file.originalname);
+  fs.rename(req.file.path, file, function(err) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    } else {
+      res.json({
+        message: 'File uploaded successfully',
+        filename: req.file.filename
+      });
+    }
+  });
+});
+
+router.get('/listphotos', function(req, res, next) {
+  var directoryPath = path.join(__dirname, '../public/capturedphotos/');
+  var jSonF = JSON.parse('[{}]');
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    var i = 0;
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        var str = "Gambar ke-" + i++;
+        jSonF.push({gambar:file});
+        console.log(file); 
+        console.log(JSON.stringify(jSonF));
+    });
+    res.render('http_se', {ga : jSonF});
+  });
 });
 
 module.exports = router;
