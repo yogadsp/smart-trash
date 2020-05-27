a3af9aea44da39fc51c1383b4b5f918f28e507f6
yogadsp <yogaseptana@gmail.com>
2020-05-27 20:53:17 +0700

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -10,36 +10,63 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/post', upload.single('file'), function(req, res) {
-  var file = path.join(__dirname, '../public/capturedphotos/', req.file.originalname);
-  fs.rename(req.file.path, file, function(err) {
-    if (err) {
-      console.log(err);
-      res.send(500);
+  var namaFile = 'gambar';
+
+  // penamaan file dengan mengurutkan dari file terakhir
+  fileTerakhir(function(namaFileTerakhir){
+    var noUrut = namaFileTerakhir.substring(6, 7);
+
+    if(noUrut == null){
+      noUrut = 1;
     } else {
-      res.json({
-        message: 'File uploaded successfully',
-        filename: req.file.filename
-      });
+      noUrut++;
+    }
+
+    var file = path.join(__dirname, '../public/capturedphotos/', namaFile + noUrut + '.jpg');
+
+    fs.rename(req.file.path, file, function(err) {
+      if (err) {
+        console.log(err);
+        res.send(500);
+      } else {
+        res.json({
+          message: 'File uploaded successfully',
+          filename: namaFile + noUrut + '.jpg'
+        });
+      }
+    });
+  });
+  
+});
+
+function fileTerakhir(callback){
+  var directoryPath = path.join(__dirname, '../public/capturedphotos/');
+  var namaFileTerakhir = null;
+  
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    } else {
+      namaFileTerakhir = files[files.length - 1];
+      callback(namaFileTerakhir);
     }
   });
-});
+}
 
 router.get('/listphotos', function(req, res, next) {
   var directoryPath = path.join(__dirname, '../public/capturedphotos/');
   var jSonF = JSON.parse('[]');
+
   fs.readdir(directoryPath, function (err, files) {
     //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
     //listing all files using forEach
-    var i = 0;
     files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        var str = "Gambar ke-" + i++;
+        // menambahkan semua gambar ke json
         jSonF.push({gambar:file});
-        console.log(file); 
-        console.log(JSON.stringify(jSonF));
     });
     res.render('http_se', {ga : jSonF});
   });
