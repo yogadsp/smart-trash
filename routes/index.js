bb19b66b70d3d089c4cbae748fc322ea4c4d5c85
yogadsp <yogaseptana@gmail.com>
2020-05-30 21:32:22 +0700

    update customvision api

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var multer  = require('multer');
 var fs  = require('fs');
 var upload = multer({ dest: '/tmp/'});
+var needle = require('needle');
 path = require('path');
 
 router.get('/', function(req, res, next) {
@@ -10,33 +11,63 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/post', upload.single('file'), function(req, res) {
-  var namaFile = 'gambar';
+  var namaFile = getWaktuSekarang();
 
-  // penamaan file dengan mengurutkan dari file terakhir
-  fileTerakhir(function(namaFileTerakhir){
-    var noUrut = namaFileTerakhir.substring(6, 7);
+  // penamaan file dengan mengambil waktu sekarang sampai dengan seconds
+  var file = path.join(__dirname, '../public/capturedphotos/', namaFile + '.jpg');
 
-    if(noUrut == null){
-      noUrut = 1;
+  fs.rename(req.file.path, file, function(err) {
+    if (err) {
+      console.log(err);
+      res.send(500);
     } else {
-      noUrut++;
+      res.json({
+        message: 'File uploaded successfully',
+        filename: namaFile + '.jpg'
+      });
     }
+  });
 
-    var file = path.join(__dirname, '../public/capturedphotos/', namaFile + noUrut + '.jpg');
-
-    fs.rename(req.file.path, file, function(err) {
-      if (err) {
-        console.log(err);
-        res.send(500);
+  var options = {
+    headers:  { 
+                'Prediction-Key': '2f1de063387a4c51b872d84e5fa04cee', 
+                'Content-Type'  : 'application/json'
+              }
+  }
+  
+  needle
+    .post(  'https://custom-vision-st.cognitiveservices.azure.com/customvision/v3.0/Prediction/861bb24c-ed41-48f7-928f-1febb1da8229/classify/iterations/Iteration1/url',
+            { "Url" : "https://mello.id/wp-content/uploads/2019/10/Sampah-Anorganik.jpg"}, options, function(err, resp) {
+      if(err){
+        console.log("ERROR : " + err);
       } else {
-        res.json({
-          message: 'File uploaded successfully',
-          filename: namaFile + noUrut + '.jpg'
-        });
+        // menangkap respon berbentuk json
+        let tempKlas = JSON.stringify(resp.body.predictions[0]);
+
+        // mendapatkan value nya saja
+        let klas = JSON.parse(tempKlas);
+        console.log("RESPON : " + i['tagName']);
       }
     });
+
+});
+
+router.get('/listphotos', function(req, res, next) {
+  var directoryPath = path.join(__dirname, '../public/capturedphotos/');
+  var jSonF = JSON.parse('[]');
+
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // menambahkan semua gambar ke json
+        jSonF.push({gambar:file});
+    });
+    res.render('http_se', {ga : jSonF});
   });
-  
 });
 
 function fileTerakhir(callback){
@@ -54,22 +85,21 @@ function fileTerakhir(callback){
   });
 }
 
-router.get('/listphotos', function(req, res, next) {
-  var directoryPath = path.join(__dirname, '../public/capturedphotos/');
-  var jSonF = JSON.parse('[]');
+function getWaktuSekarang(){
+  let dateObj = new Date(Date.now());
 
-  fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // menambahkan semua gambar ke json
-        jSonF.push({gambar:file});
-    });
-    res.render('http_se', {ga : jSonF});
-  });
-});
+  // mendapatkan waktu sekarang
+  // '0' agar ada tambahan angka 0 jika terjadi 1 digit, misal 5 => 05
+  let date = ("0" + datObj.getDate()).slice(-2);
+  let month = ("0" + (datObj.getMonth() + 1)).slice(-2);
+  let year = datObj.getFullYear();
+  let hours = ("0" + dateObj.getHours()).slice(-2);
+  let minutes = ("0" + dateObj.getMinutes()).slice(-2);;
+  let seconds = ("0" + dateObj.getSeconds()).slice(-2);
+
+  let waktuSekarang = year + month + date + hours + minutes + seconds;
+
+  return waktuSekarang;
+}
 
 module.exports = router;
