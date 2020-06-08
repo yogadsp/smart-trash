b64fa68666ff7d73155fb63bfecdb48edf0fb8d2
yogadsp <yogaseptana@gmail.com>
2020-06-08 09:55:25 +0700

    update index.js for collect dataset

--- routes/index.js
+++ routes/index.js
@@ -31,6 +31,70 @@ router.get('/', function(req, res, next) {
 // global variable untuk link gambar
 var namaFile = null;
 
+// POST + classification
+// router.post('/post', upload.single('file'), function(req, res, next) {
+//   namaFile = getWaktuSekarang();
+
+//   // penamaan file dengan mengambil waktu sekarang sampai dengan seconds
+//   var file = path.join(__dirname, '../public/capturedphotos/', namaFile + '.jpg');
+
+//   fs.rename(req.file.path, file, function(err) {
+//     if (err) {
+//       console.log(err);
+//       res.send(500);
+//     }
+//   });
+
+//   // membuat aksi lanjutan setelah mengeksekusi route /post
+//   next()
+// }, function(req, res){
+//   var options = {
+//     headers:  { 
+//                 'Prediction-Key': '2f1de063387a4c51b872d84e5fa04cee', 
+//                 'Content-Type'  : 'application/json'
+//               }
+//   }
+//   // "http://52.163.219.128/capturedphotos/" + namaFile + ".jpg"
+//   needle
+//     .post(  'https://custom-vision-st.cognitiveservices.azure.com/customvision/v3.0/Prediction/861bb24c-ed41-48f7-928f-1febb1da8229/classify/iterations/Iteration1/url',
+//             { "Url" : "https://mello.id/wp-content/uploads/2019/10/Sampah-Anorganik.jpg"}, options, function(err, resp) {
+//       if(err){
+//         console.log("ERROR : " + err);
+//       } else {
+//         // menangkap respon berbentuk json
+//         let tempKlas = JSON.stringify(resp.body.predictions[0]);
+
+//         // mendapatkan value nya saja
+//         let klas = JSON.parse(tempKlas);
+//         console.log("RESPON : " + klas['tagName']);
+
+//         client.publish('klasif1', klas['tagName'], function() {
+//           // res.writeHead(204, { 'Connection': 'keep-alive' });
+//           // res.end();
+//           dbFile[0].klasifikasi = klas['tagName'];
+
+//           // null - represents the replacer function. (in this case we don't want to alter the process)
+//           // 2 - represents the spaces to indent.
+//           fs.writeFile(dbFileName, JSON.stringify(dbFile, null, 2), function writeJSON(err) {
+//             if (err) return console.log(err);
+//             console.log(JSON.stringify(dbFile));
+//             console.log('writing to ' + dbFileName);
+//           });
+
+//           console.log(klas['tagName'] + ' Published!');
+//         });
+
+//         res.json({
+//           message: 'File uploaded successfully',
+//           filename: namaFile + '.jpg',
+//           tagName : klas['tagName']
+//         });
+//       }
+//     });
+// });
+
+
+// ====== POST SAJA ========
 router.post('/post', upload.single('file'), function(req, res, next) {
   namaFile = getWaktuSekarang();
 
@@ -41,55 +105,13 @@ router.post('/post', upload.single('file'), function(req, res, next) {
     if (err) {
       console.log(err);
       res.send(500);
+    } else {
+      res.json({
+        message: 'File uploaded successfully',
+        filename: namaFile + '.jpg'
+      });
     }
   });
-
-  // membuat aksi lanjutan setelah mengeksekusi route /post
-  next()
-}, function(req, res){
-  var options = {
-    headers:  { 
-                'Prediction-Key': '2f1de063387a4c51b872d84e5fa04cee', 
-                'Content-Type'  : 'application/json'
-              }
-  }
-  // "http://52.163.219.128/capturedphotos/" + namaFile + ".jpg"
-  needle
-    .post(  'https://custom-vision-st.cognitiveservices.azure.com/customvision/v3.0/Prediction/861bb24c-ed41-48f7-928f-1febb1da8229/classify/iterations/Iteration1/url',
-            { "Url" : "https://mello.id/wp-content/uploads/2019/10/Sampah-Anorganik.jpg"}, options, function(err, resp) {
-      if(err){
-        console.log("ERROR : " + err);
-      } else {
-        // menangkap respon berbentuk json
-        let tempKlas = JSON.stringify(resp.body.predictions[0]);
-
-        // mendapatkan value nya saja
-        let klas = JSON.parse(tempKlas);
-        console.log("RESPON : " + klas['tagName']);
-
-        client.publish('klasif1', klas['tagName'], function() {
-          // res.writeHead(204, { 'Connection': 'keep-alive' });
-          // res.end();
-          dbFile[0].klasifikasi = klas['tagName'];
-
-          // null - represents the replacer function. (in this case we don't want to alter the process)
-          // 2 - represents the spaces to indent.
-          fs.writeFile(dbFileName, JSON.stringify(dbFile, null, 2), function writeJSON(err) {
-            if (err) return console.log(err);
-            console.log(JSON.stringify(dbFile));
-            console.log('writing to ' + dbFileName);
-          });
-
-          console.log(klas['tagName'] + ' Published!');
-        });
-
-        res.json({
-          message: 'File uploaded successfully',
-          filename: namaFile + '.jpg',
-          tagName : klas['tagName']
-        });
-      }
-    });
 });
 
 router.get('/listphotos', function(req, res, next) {
